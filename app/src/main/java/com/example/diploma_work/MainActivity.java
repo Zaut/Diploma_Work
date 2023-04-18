98b0000334b4efd8ef91fb92808e46c23f24d439
tolstenkaya <o.tutova.82@gmail.com>
2023-04-18 22:11:48 +0300

    add fragment and desigen

--- app/src/main/java/com/example/diploma_work/MainActivity.java
+++ app/src/main/java/com/example/diploma_work/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.diploma_work;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+
+        FragmentRegistration myFragment = new FragmentRegistration();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.my_fragment, myFragment);
+        fragmentTransaction.commit();
     }
 }
