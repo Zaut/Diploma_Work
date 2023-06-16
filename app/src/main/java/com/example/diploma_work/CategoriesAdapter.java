9a527bd0d9b0f9abc5a0eacfd2eda6cdbcd0d22d
Zaut <kisluy51@gmail.com>
2023-06-16 03:48:42 +0300

    added clearing the fields of the passed category to a separate thread and also added a progress bar for waiting

--- app/src/main/java/com/example/diploma_work/CategoriesAdapter.java
+++ app/src/main/java/com/example/diploma_work/CategoriesAdapter.java
@@ -1,9 +1,11 @@
 package com.example.diploma_work;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -23,9 +26,8 @@ public class CategoriesAdapter extends BaseAdapter {
     private List<Categories> data;
     private LayoutInflater inflater;
 
+    private ProgressDialog progressDialog;
     private FragmentManager fragmentManager;
-
-    private String name;
 
     private Context context;
 
@@ -55,7 +57,6 @@ public class CategoriesAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder;
         if (convertView == null) {
-
             convertView = inflater.inflate(R.layout.categories_list, parent, false);
             holder = new ViewHolder();
             holder.button = convertView.findViewById(R.id.btn_categories_list_item);
@@ -68,103 +69,90 @@ public class CategoriesAdapter extends BaseAdapter {
         holder.button.setText(category.CategoriesName);
 
         if (GlobalVariables.listSucces.contains(category.CategoriesName)) {
-
             holder.button.setBackgroundResource(R.drawable.button_succes);
-
         } else {
-
             holder.button.setBackgroundResource(R.drawable.button_level);
-
         }
+
         holder.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-
                 String selectedCategory = category.CategoriesName;
                 if (GlobalVariables.listSucces.contains(selectedCategory)) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                    builder.setTitle("Начать сначала");
-                    builder.setMessage("Хотите начать сначала?");
-                    builder.setPositiveButton("Да", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            ConnectionHelper connectionHelper = new ConnectionHelper();
-                            GetData getData = new GetData();
-                            List<Words> words = getData.createNullWords(category.CategoriesName);
-
-                            for (Words word : words) {
-                                Log.e("word.CategoryName", word.CategoryName + ": ");
-                                word.setCompleted(0);
-                                connectionHelper.updateWordCompletionStatus(word.Words, word.getCompleted(), selectedCategory);
-                                GlobalVariables.listSucces.remove(selectedCategory);
-
-                            }
-                            Categories category = data.get(position);
-                            Fragment wordsFragment = new WordsFragment();
-                            Bundle args = new Bundle();
-                            args.putString("selectedCategories", category.CategoriesName);
-                            wordsFragment.setArguments(args);
-
-                            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                            fragmentTransaction.replace(R.id.Frame_Layout, wordsFragment);
-                            fragmentTransaction.addToBackStack(null);
-                            fragmentTransaction.commit();
-
-
-                        }
-                    });
-                    builder.setNegativeButton("Нет", null);
-
-                    AlertDialog dialog = builder.create();
-                    dialog.show();
-                }
-                else{
-                    Categories category = data.get(position);
-
-
-                Fragment wordsFragment = new WordsFragment();
-                Bundle args = new Bundle();
-                args.putString("selectedCategories", category.CategoriesName);
-                wordsFragment.setArguments(args);
-
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.Frame_Layout, wordsFragment);
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
+                    showResetConfirmationDialog(selectedCategory);
+                } else {
+                    startWordsFragment(category.CategoriesName);
                 }
             }
-
         });
-//                Categories category = data.get(position);
-//
-//                // Создаем новый фрагмент для отображения категорий
-//                Fragment wordsFragment = new WordsFragment();
-//
-//                // Передаем информацию о выбранном уровне во фрагмент
-//                Bundle args = new Bundle();
-//                args.putString("selectedCategories", category.CategoriesName);
-//                wordsFragment.setArguments(args);
-//
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                fragmentTransaction.replace(R.id.Frame_Layout, wordsFragment);
-//                fragmentTransaction.addToBackStack(null);
-//                fragmentTransaction.commit();
-//            }
-//        });
+
         return convertView;
+    }
+
+    private void startWordsFragment(String selectedCategory) {
+        Fragment wordsFragment = new WordsFragment();
+        Bundle args = new Bundle();
+        args.putString("selectedCategories", selectedCategory);
+        wordsFragment.setArguments(args);
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.Frame_Layout, wordsFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    private void showResetConfirmationDialog(String selectedCategory) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Начать сначала");
+        builder.setMessage("Хотите начать сначала?");
+        builder.setPositiveButton("Да", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                progressDialog = new ProgressDialog(context);
+                progressDialog.setTitle("Сброс данных");
+                progressDialog.setMessage("Выполняется сброс данных...");
+                progressDialog.setIndeterminate(false);
+                progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                progressDialog.show();
+
+                new ResetDataTask().execute(selectedCategory);
+            }
+        });
+        builder.setNegativeButton("Нет", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private class ResetDataTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String  doInBackground(String... params) {
+            String selectedCategory = params[0];
+            ConnectionHelper connectionHelper = new ConnectionHelper();
+            GetData getData = new GetData();
+            List<Words> words = getData.createNullWords(selectedCategory);
+
+            for (Words word : words) {
+                Log.e("word.CategoryName", word.CategoryName + ": ");
+                word.setCompleted(0);
+                connectionHelper.updateWordCompletionStatus(word.Words, word.getCompleted(), selectedCategory);
+                GlobalVariables.listSucces.remove(selectedCategory);
+            }
+
+            return selectedCategory;
+        }
+
+        @Override
+        protected void onPostExecute(String selectedCategory) {
+            progressDialog.dismiss();
+            Toast.makeText(context, "Данные успешно сброшены", Toast.LENGTH_SHORT).show();
+            startWordsFragment(selectedCategory);
+
+        }
     }
 
     private static class ViewHolder {
         Button button;
     }
-
-    private void iterateList() {
-        for (String element : GlobalVariables.listSucces)
-        {
-
-        }
-    }
-
 }
